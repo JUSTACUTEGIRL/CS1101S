26b3e5b049f9789c0134d01361ff010c97d10157
Nguyen Quang Minh <No public email provided>
2023-10-13 15:49:24 +0800

    Changes made from Source Academy

--- robot.js
+++ robot.js
@@ -2,54 +2,95 @@ const motorA = ev3_motorA();
 const motorB = ev3_motorB();
 const ultrasound = ev3_ultrasonicSensor();
 
-function turn(degree) {
-    function turn_90() {
-        const time = 500;
-        ev3_runForTime(motorA, time, 250);
-        ev3_runForTime(motorB, time, -250);
+function turn_anticlockwise(degree) {
+    function turn_anticlockwise_90() {
+        const time = 900;
+        ev3_runForTime(motorA, time, 150);
+        ev3_runForTime(motorB, time, -150);
         ev3_pause(time + 200);
     }
     const time = math_floor(degree / 90);
     
     for (let i = 0; i < time; i = i + 1) {
-        turn_90();
+        turn_anticlockwise_90();
+    }
+}
+
+function turn_clockwise(degree) {
+    function turn_clockwise_90() {
+        const time = 900;
+        ev3_runForTime(motorA, time, -150);
+        ev3_runForTime(motorB, time, 150);
+        ev3_pause(time + 400);
+    }
+    const time = math_floor(degree / 90);
+    
+    for (let i = 0; i < time; i = i + 1) {
+        turn_clockwise_90();
     }
 }
 
 function move(distance) {
     function move_5cm() {
         const time = 440;
-        ev3_runForTime(motorA, time, 200);
-        ev3_runForTime(motorB, time, 200);
+        ev3_runForTime(motorA, time, 250);
+        ev3_runForTime(motorB, time, 250);
         ev3_pause(time + 200);
     }
     
-    const time = math_floor(distance / 5 - 1);
+    const time = math_floor(distance / 5);
     
     for (let i = 0; i < time; i = i + 1) {
         move_5cm();
     }
 }
 
-
 function move_ultrasound() {
     const distance = ev3_ultrasonicSensorDistance(ultrasound);
     display(distance);
-    move((distance - 80) / 10);
+    if (distance < 1000) {
+        move((distance - 200) / 10);
+    }
 }
 
-function navigate() {
+function navigate_left() {
     move_ultrasound();
     while(ev3_ultrasonicSensorDistance(ultrasound) < 1000) {
-        turn(90);
-        move(20);
-        turn(270);
+        turn_anticlockwise(90);
+        move(15);
+        turn_clockwise(90);
     }
-    turn(90);
-    move(20);
-    turn(270);
+    turn_anticlockwise(90);
+    move(10);
+    turn_clockwise(90);
     move(50);
 }
 
-navigate();
+function navigate_right() {
+    move_ultrasound();
+    while(ev3_ultrasonicSensorDistance(ultrasound) < 1000) {
+        turn_clockwise(90);
+        move(15);
+        turn_anticlockwise(90);
+    }
+    move(50);
+}
 
+function navigate() {
+    const ran = math_random();
+    display(ran);
+    if (ran <= 0.5) {
+        navigate_left();
+    } else {
+        navigate_right();
+    }
+}
+
+function every_second() {
+    for (let i = 0; i < 20; i = i + 1) {
+        display(ev3_ultrasonicSensorDistance(ultrasound));
+        ev3_pause(1000);
+    }
+}
+
+navigate();
