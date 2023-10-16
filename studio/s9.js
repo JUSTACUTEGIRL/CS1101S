3984cda6a7f78145776aed14d7ca8653235c88af
Nguyen Quang Minh <No public email provided>
2023-10-16 21:11:31 +0800

    update studio s9

--- studio/s9.js
+++ studio/s9.js
@@ -11,6 +11,10 @@ function d_filter(pred, xs) {
     let previous = xs;
     let current = xs;
     let first = true;
+    
+    if (is_null(xs)) {
+        return null;
+    }
 
     while (!is_null(current)) {
         if (pred(head(current))) {
@@ -23,16 +27,20 @@ function d_filter(pred, xs) {
                 set_tail(previous, current);
             }
             
-            previous = tail(previous);
+            previous = current;
         }
         
         current = tail(current);
+    }
+    
+    if (first === true) {
+        return null;
     }
     
     set_tail(previous, null);
     return xs;
 }
 
-d_filter(x => x % 2 === 0, L);
+//d_filter(x => x % 2 === 0, L);
 
-
+d_filter(x => x / 5 === 2, L);
